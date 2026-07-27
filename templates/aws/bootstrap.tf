2ef99b4b56d3c791d79a4dde9cdf869990e723fa
Devopstrio <engineering@devopstrio.co.uk>
2026-07-27 16:33:08 +0530

    infra(terraform): add AWS and Azure S3/SA state storage templates

---
+++ templates/aws/bootstrap.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket        = "devopstrio-tfstate-bootstrap-bucket"
+  force_destroy = false
+
+  tags = {
+    Name        = "Terraform State Storage"
+    Environment = "production"
+    ManagedBy   = "Devopstrio-Cloud-Bootstrap-Kit"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "devopstrio-tflocks-bootstrap-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ templates/azure/bootstrap.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "tfstate_rg" {
+  name     = "rg-tfstate-bootstrap"
+  location = "westeurope"
+}
+
+resource "azurerm_storage_account" "tfstate_sa" {
+  name                     = "devopstriobootstrapstate"
+  resource_group_name      = azurerm_resource_group.tfstate_rg.name
+  location                 = azurerm_resource_group.tfstate_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
